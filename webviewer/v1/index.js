27f637530dd374c2e0434962a115d4ee328f5ee3
Andrey Safonov <a.so777@live.ca>
2021-08-03 16:40:41 -0700

    Updated component to allow loading of multiple documents

--- webviewer/v1/index.js
+++ webviewer/v1/index.js
@@ -8,6 +8,61 @@ Appian.Component.onNewValue(function (newValues) {
     return;
   }
 
+  async function getDocumentFromAppian(docId) {
+    var docData, docName, message;
+    function handleClientApiResponse(response) {
+      if (response.payload.error) {
+        console.error(
+          "Connected system response: " + response.payload.error
+        );
+        Appian.Component.setValidations(
+          "Connected system response: " + response.payload.error
+        );
+        return;
+      }
+      docData = response.payload.docData;
+      docName = response.payload.docName;
+
+      if (checkNull(docData) || checkNull(docName)) {
+        message = "Unable to obtain the doc data from the connected system";
+        console.error(message);
+        Appian.Component.setValidations(message);
+        return;
+      } else {
+        // Clear any error messages
+        Appian.Component.setValidations([]);
+      }
+    }
+
+    function handleError(response) {
+      if (response.error && response.error[0]) {
+        console.error(response.error);
+        Appian.Component.setValidations([response.error]);
+      } else {
+        message = "An unspecified error occurred";
+        console.error(message);
+        Appian.Component.setValidations(message);
+      }
+    }
+
+    const payload = {
+      documentId: docId,
+    };
+
+    await Appian.Component.invokeClientApi(
+      docAccessConnectedSystem,
+      "WebViewerRetrieveClientApi",
+      payload
+    )
+      .then(handleClientApiResponse)
+      .catch(handleError);
+
+    return {
+      docBase64: docData,
+      docName,
+    };
+  }
+
   WebViewer(
     {
       path: "/suite/rest/a/content/latest/webcontent/webviewer/lib/",
@@ -17,7 +72,7 @@ Appian.Component.onNewValue(function (newValues) {
     },
     document.getElementById("viewer")
   ).then((instance) => {
-    const { docViewer, annotManager } = instance;
+    const { docViewer, annotManager, CoreControls } = instance;
 
     instance.setHeaderItems((header) => {
       header.push({
@@ -90,87 +145,97 @@ Appian.Component.onNewValue(function (newValues) {
     });
 
     if (!checkNull(appianDocId)) {
-      async function getDocumentFromAppian() {
-        var docData, docName, message;
-        function handleClientApiResponse(response) {
-          if (response.payload.error) {
-            console.error(
-              "Connected system response: " + response.payload.error
-            );
-            Appian.Component.setValidations(
-              "Connected system response: " + response.payload.error
-            );
-            return;
-          }
-          docData = response.payload.docData;
-          docName = response.payload.docName;
-
-          if (checkNull(docData) || checkNull(docName)) {
-            message = "Unable to obtain the doc data from the connected system";
-            console.error(message);
-            Appian.Component.setValidations(message);
-            return;
-          } else {
-            // Clear any error messages
-            Appian.Component.setValidations([]);
-          }
-        }
-
-        function handleError(response) {
-          if (response.error && response.error[0]) {
-            console.error(response.error);
-            Appian.Component.setValidations([response.error]);
-          } else {
-            message = "An unspecified error occurred";
-            console.error(message);
-            Appian.Component.setValidations(message);
-          }
-        }
-
-        const payload = {
-          documentId: appianDocId,
-        };
-
-        await Appian.Component.invokeClientApi(
-          docAccessConnectedSystem,
-          "WebViewerRetrieveClientApi",
-          payload
-        )
-          .then(handleClientApiResponse)
-          .catch(handleError);
-
-        return {
-          docBase64: docData,
-          docName,
-        };
-      }
-
-      getDocumentFromAppian().then(
-        function (documentData) {
-          if (
-            checkNull(documentData.docBase64) ||
-            checkNull(documentData.docName)
-          ) {
+      if (appianDocId.toString().split(',').length === 1) {
+        getDocumentFromAppian(Number(appianDocId)).then(
+          function (documentData) {
+            if (
+              checkNull(documentData.docBase64) ||
+              checkNull(documentData.docName)
+            ) {
+              Appian.Component.setValidations(
+                "Unable to fetch document from Appian"
+              );
+              return;
+            } else {
+              convertBase64ToArrayBuffer(documentData.docBase64).then(
+                (documentBuffer) => {
+                  instance.loadDocument(documentBuffer);
+                }
+              );
+              documentName = documentData.docName;
+            }
+          },
+          function (error) {
             Appian.Component.setValidations(
               "Unable to fetch document from Appian"
             );
-            return;
-          } else {
-            convertBase64ToArrayBuffer(documentData.docBase64).then(
-              (documentBuffer) => {
-                instance.loadDocument(documentBuffer);
-              }
-            );
-            documentName = documentData.docName;
+            console.error(error);
           }
-        },
-        function (error) {
-          Appian.Component.setValidations(
-            "Unable to fetch document from Appian"
-          );
-          console.error(error);
-        }
-      );
+        );
+      } else if (appianDocId.toString().split(',').length > 1) {
+        let promiseArray = [];
+        appianDocId.split(',').forEach(id => {
+          promiseArray.push(getDocumentFromAppian(Number(id)));
+        });
+        Promise.all(promiseArray).then(values => {
+          let blobPromiseArray = [];
+
+          values.forEach(function (documentData) {
+            if (
+              checkNull(documentData.docBase64) ||
+              checkNull(documentData.docName)
+            ) {
+              Appian.Component.setValidations(
+                "Unable to fetch document from Appian"
+              );
+              return;
+            } else {
+              blobPromiseArray.push(convertBase64ToArrayBuffer(documentData.docBase64));
+              Promise.all(blobPromiseArray).then(values => {
+                mergeDocuments(values).then(mergedPdf => {
+                  // merged pdf, here you can download it using mergedPdf.getFileData
+                  instance.loadDocument(mergedPdf);
+                });
+                
+                // recursive function with promise 
+                function mergeDocuments(urlArray, nextCount = 1, doc = null) {
+                  return new Promise(async function(resolve, reject) {
+                    if (!doc) {
+                      doc = await CoreControls.createDocument(urlArray[0]);
+                    }
+                    const newDoc = await CoreControls.createDocument(urlArray[nextCount]);
+                    const newDocPageCount = newDoc.getPageCount();
+                
+                    // create an array containing 1…N
+                    const pages = Array.from({ length: newDocPageCount }, (v, k) => k + 1);
+                    const pageIndexToInsert = doc.getPageCount() + 1;
+                    // in this example doc.getPageCount() returns 3
+                
+                    doc.insertPages(newDoc, pages, pageIndexToInsert)
+                      .then(result => resolve({
+                        next: urlArray.length - 1 > nextCount,
+                        doc: doc,
+                      })
+                    );
+                    // end Promise
+                  }).then(res => {
+                    return res.next ?
+                      mergeDocuments(urlArray, nextCount + 1, res.doc) :
+                      res.doc;
+                  });
+                }
+              });
+            }
+          },
+          function (error) {
+            Appian.Component.setValidations(
+              "Unable to fetch document from Appian"
+            );
+            console.error(error);
+          });
+        });
+      }
+      
     }
 
     docViewer.on("documentLoaded", () => {
